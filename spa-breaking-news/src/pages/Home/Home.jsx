074cc11eb37e2a3722e2be05a475be98c5e5d510
davidsnts <david.santos@delage.com.br>
2024-10-19 10:21:43 -0300

    Global styled component

--- spa-breaking-news/src/pages/Home/Home.jsx
+++ spa-breaking-news/src/pages/Home/Home.jsx
@@ -1,11 +1,10 @@
 import { Navbar } from "../../components/navbar/Navbar";
-import { Button } from "../../components/navbar/NavbarStyled";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <Button>Olá, Home</Button>
+      <h1>Olá, Home</h1>
     </>
   );
 }
